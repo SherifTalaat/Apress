16d2451ee105bfc66bc853114a98c1dc9290ae24
Sherif Talaat <sherif.talaat@innovation-hut.com>
2016-08-20 07:23:46 +0000

    Initialte upload

---
+++ SimpleCalculator.ps1
@@ -0,0 +1,17 @@
+﻿#Calculator Script Module
+
+Function Add-Numbers($x,$y)
+{
+    $x + $y
+}
+
+Function Subtract-Numbers($x,$y)
+{
+    $x - $y
+}
+
+New-Alias -Name an -Value Add-Numbers
+New-Alias -Name sn -Value Subtract-Numbers
+
+#Export modules member
+Export-ModuleMember -Function * -Alias *
---
+++ x1.ps1
@@ -0,0 +1,1 @@
+﻿Get-Service | Out-File C:\myData\services.txt
